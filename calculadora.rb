613f6729c776ea7bb6c62179c00a86a58729c23a
SandroPaiva <sandroneripaiva@gmail.com>
2021-08-30 18:32:36 -0300

    Comando Calculadora em Ruby

---
+++ calculadora.rb
@@ -0,0 +1,21 @@
+def soma(primeiro_numero, segundo_numero)
+    primeiro_numero + segundo_numero
+end
+
+def subtracao(primeiro_numero, segundo_numero)
+    primeiro_numero - segundo_numero
+end
+
+def multiplicacao (x,y)
+    x * y
+end
+
+def divisao (a,b)
+
+    if b == 0
+        return 'Opa! Zero como divisor'
+    else
+        a / b
+    end
+       
+end
